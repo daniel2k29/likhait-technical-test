46c23111bec6675bed597488b8f9110b6c26ae7f
Daniel <danielpaulobayot@gmail.com>
2026-03-02 22:53:54 +0800

    Fix Categories API by correcting syntax in CategoriesController

--- backend/app/controllers/api/categories_controller.rb
+++ backend/app/controllers/api/categories_controller.rb
@@ -12,9 +12,11 @@ class Api::CategoriesController < ApplicationController
     else
       render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  private
 
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
